65b200737944672dd34c7441b02a9edadefc0d12
Vick Kumar <vick.kumar44@gmail.com>
2025-07-11 18:42:28 -0400

    added rules

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,19 @@
+defmodule Raffley.Rules do
+
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participants should have a high tolerance for puns and dad jokes"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming the prize"
+      },
+      %{
+        id: 3,
+        text: "Have fun!"
+      }
+    ]
+  end
+end
--- lib/raffley_web/controllers/rule_controller.ex
+++ lib/raffley_web/controllers/rule_controller.ex
@@ -1,8 +1,11 @@
 defmodule RaffleyWeb.RuleController do
   use RaffleyWeb, :controller
 
+  alias Raffley.Rules
+
   def index(conn, _params) do
     emojis = ~w(👀 🙈 😏) |> Enum.random() |> String.duplicate(5)
-    render(conn, :index, emojis: emojis)
+    rules = Rules.list_rules()
+    render(conn, :index, emojis: emojis, rules: rules)
   end
 end
